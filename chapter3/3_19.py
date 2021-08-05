829511daa29c4907791e457259164434d54335f6
Dorumuk <dongyun.lee@clbee.com>
2021-08-05 17:14:07 +0900

    3_19.

--- chapter3/3_19.py
+++ chapter3/3_19.py
@@ -1,15 +1,12 @@
-from itertools import permutations
+from itertools import combinations, permutations
 # N : num of sequence
 
 def add (p_x, p_y): return p_x + p_y
-
-def subtract (p_x, p_y): return p_x - p_y
-  
+def subtract (p_x, p_y): return p_x - p_y  
 def multiply (p_x, p_y): return p_x * p_y
-
 def devide (p_x, p_y): return p_x / p_y
 
-def get_input_data ():
+def getInputData ():
   return int(input()), input().split(), input().split()
 
 def getOprList ( opr ):
@@ -18,24 +15,30 @@ def getOprList ( opr ):
     tmp_list += [idx for i in range(int(op))]
   return list(permutations(tmp_list, len(tmp_list)))
 
+def run ( new_list : list, result ):
+  for j, opr in enumerate(new_list):
+    v = int(seq[j + 1])
+    # print(result, v)
+    if(opr == 0): result = add(result, v)
+    elif (opr == 1): result = subtract(result, v)
+    elif (opr == 2): result = multiply(result, v)
+    elif (opr == 3): result = devide(result, v)
+  return result
+
 def start ():
-  num_of_seq, seq, operators = get_input_data()
-  global result
+  # global result
+  container = []
   opr_lists = getOprList(operators)
-  for idx, value in enumerate(seq):
-    v = int(value)
-    if(idx > 0):
-      for i, opr_list in enumerate(opr_lists):
-        print(opr_list)
-        for j, opr in enumerate(list(opr_list)):
-          print(opr)
-          if(opr == 0): result = add(v, result)
-          elif (opr == 1): result = subtract(v, result)
-          elif (opr == 2): result = multiply(v, result)
-          elif (opr == 3): result = devide(v, result)
-        print(">>"+str(result))
-    result = v
-  print(result)
+  print("-----------------",opr_lists)  # opr_lists: ( + - - * +), ( - + - * +), ( + - - * +)
+  for i, opr_list in enumerate(opr_lists):
+    # print("-----------------",opr_list) # opr_list: ( + - - * +)
+    result = int(seq[0])
+    container.append(run(list(opr_list), result))
+    # container.append(run(reversed(list(opr_list)), result))
+  print(container)
+  print(min(container))
+  print(max(container))
 
 
+num_of_seq, seq, operators = getInputData()
 start()
